f856090fc47841bfcaebbef5acc4d53d948e9935
Anees Baqir <anees.baqir@outlook.com>
2021-06-08 16:37:36 +0500

    basic exoplayer

---
+++ app/src/main/java/com/iu/vodlix/subtitlesharing/MainActivity.java
@@ -0,0 +1,105 @@
+package com.iu.vodlix.subtitlesharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class MainActivity extends AppCompatActivity {
+
+    PlayerView playerView;
+    ProgressBar loading;
+    private TrackSelector mTrackSelector;
+    private SimpleExoPlayer player;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playerView = findViewById(R.id.video_view);
+        loading = findViewById(R.id.loading);
+
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        mTrackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        player = ExoPlayerFactory.newSimpleInstance(this, mTrackSelector);
+        playerView.setPlayer(player);
+
+
+        String userAgent = Util.getUserAgent(this, "PlaybackVideoFragment");
+
+        String hls_url = "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8";
+        MediaSource  videoSource =
+                new HlsMediaSource.Factory(new DefaultDataSourceFactory(this, userAgent)).createMediaSource(Uri.parse(hls_url));
+        player.prepare(videoSource);
+        player.setPlayWhenReady(true);
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+            }
+            @Override
+            public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+            }
+            @Override
+            public void onLoadingChanged(boolean isLoading) {
+            }
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                switch (playbackState) {
+                    case ExoPlayer.STATE_READY:
+                        loading.setVisibility(View.GONE);
+                        break;
+                    case ExoPlayer.STATE_BUFFERING:
+                        loading.setVisibility(View.VISIBLE);
+                        break;
+                }
+            }
+            @Override
+            public void onRepeatModeChanged(int repeatMode) {
+            }
+            @Override
+            public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+            }
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+            }
+            @Override
+            public void onPositionDiscontinuity(int reason) {
+            }
+            @Override
+            public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+            }
+            @Override
+            public void onSeekProcessed() {
+            }
+        });
+
+    }
+}
